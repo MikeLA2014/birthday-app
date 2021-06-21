2786572a009ddb177b98a17bdfe879595c26244d
Mike Alves <alvesmla2014@gmail.Com>
2021-06-21 15:54:52 +0200

    merci imane

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-const btnSend = document.querySelectorAll('button')[0]
-const btnCancel = document.querySelectorAll('button')[1]
-const form = document.querySelector('form')
-const input1 = document.querySelectorAll('input')[0]
-const input2 = document.querySelectorAll('input')[1]
-const input3 = document.querySelectorAll('input')[2]
-
-// 1ere fonctionnalité
-
-btnCancel.addEventListener('click', function () {
-    input1.value = ''
-    input2.value = ''
-})
-// 2eme fonctionnalité
-
-form.addEventListener('submit', function (evt) {
-    evt.preventDefault()
- $('section').append(`<div class="card mt-3">
-<div class="card-body">
-    <p class="card-text"><span style="color:${input3.value}">${input2.value}</span> - ${input1.value}</p>
-
-</div>
-
-</div>`)
-})
+function refresh() {
+    $("section").html("")
+    var list = JSON.parse(localStorage.getItem("anniversaires"))
+    if (list) {
+      for (var i = 0; i < list.length; i++) {
+        $("section").append(`
+          <div class="card mb-3">
+            <div class="card-body">
+              <span style="color: ${list[i].color}">${list[i].firstname}</span> - Le  ${list[i].date}
+            </div>
+          </div>
+        `)
+      }
+    }
+  }
+  
+  refresh()
+  
+  $("form").on("submit", function (e) {
+    e.preventDefault()
+    var newBirthday = {
+      firstname: $("input:eq(1)").val(),
+      date: $("input:first").val(),
+      color: $("input:last").val(),
+    }
+  
+    var list = JSON.parse(localStorage.getItem("anniversaires"))
+    if (!list) {
+      localStorage.setItem("anniversaires", JSON.stringify([newBirthday]))
+      refresh()
+    } else {
+      list.push(newBirthday)
+      localStorage.setItem("anniversaires", JSON.stringify(list))
+      refresh()
+    }
+  })
+  
+  $("button:last").on("click", function () {
+    $("input:first").val("")
+    $("input:last").val("")
+  })
+  
