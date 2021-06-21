2fda0f8f2413aee2f1939055510d2a36a5b346e0
Mike Alves <alvesmla2014@gmail.Com>
2021-06-21 14:27:17 +0200

    message

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btnCancel = document.querySelectorAll('button')[1]
 const form = document.querySelector('form')
 const input1 = document.querySelectorAll('input')[0]
 const input2 = document.querySelectorAll('input')[1]
-const section = document.querySelector('section')
+const input3 = document.querySelectorAll('input')[2]
 
 // 1ere fonctionnalité
 
@@ -17,7 +17,7 @@ form.addEventListener('submit', function (evt) {
     evt.preventDefault()
  $('section').append(`<div class="card mt-3">
 <div class="card-body">
-    <p class="card-text">${input2.value} - ${input1.value}</p>
+    <p class="card-text"><span style="color:${input3.value}">${input2.value}</span> - ${input1.value}</p>
 
 </div>
 
